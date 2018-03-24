51af4f7967aad3d29e65461e843297ab0ae74718
ALX-04 <37358990+ALX-04@users.noreply.github.com>
2018-03-24 10:47:53 +0000

    Add files via upload

---
+++ AVARspam.py
@@ -0,0 +1,137 @@
+#!/usr/bin/python
+
+import os 
+import random 
+import sys 
+import time 
+import socket
+
+# colour
+
+G = "\033[32m"; O = "\033[33m"; B = "\033[36m"; R = "\033[31m"; W = "\033[0m"; P = "\033[35m";
+
+def mengetik(s):
+    for c in s + '\n':
+        sys.stdout.write(c)
+        sys.stdout.flush()
+        time.sleep(random.random() * 0.2)
+mengetik('^_< INI ALX-04 MHANK YG MASIH NEWBIE ITULOH....#NEewbieBerkarya')
+key = raw_input("pencet enter Klo mau lanjut...Klo gk mau lambaikan tangan ke kamera :v")
+
+os.system('figlet -f slant AVARspam')
+           
+print G+("                 //\\            //\\                ")
+print O+("                //  \\          //  \\               ")
+print G+("                                                     ")
+print O+("                        ------                      ")
+print R+ ("#====================================================#")
+print W+ ("#____________________________________________________#")
+print B+("|                                   ___   _    _     |")
+print P+("|         |\     /\   |  \   /     / _ \ | |  | |    |")
+print B+("|         ||    /__\  |   \ /      || || | |__| |    |")
+print P+("| ()#####| [====================================>>>  |")
+print B+("|         ||   /    \ |___ /\      ||_|| |____| |    |")
+print P+("|         |/              /  \     \___/      | |    |")
+print B+("|                        /    \               |_|    |")
+print P+("|                                                    |")
+print W+("|____________________________________________________|")
+print R+("#                                                    # ")
+print W+("#====================================================# ")
+print ("#                                                    #")
+print R+("# SmS Boomber BY ALX-04 FROM Indonesian Error System # ")
+print W+("#               GUNAKAN TOLS DENGAN BIJAK            #  ")
+print R+("#                      ( FAMILY )                    #  ")
+print W+("#    | Mr.:/R1zK1 | CIPPEL | r1c3-4 | AMRiezZ |      #  ")
+print R+("#    | Mr.BL4NGK0N4N | M4RV3L-23 | IRFAN | ALX-04    #  ")
+print W+("#    |N4ST4R_ID | UKHWAH ISLAMIYAH | Mr.3V0LUT10N    #  ")
+print R+("#    | BOY NAINGGOLAN | BITCHIS | ALL MEMBER IES     #  ")
+print B+("#                      THANKS TO                     #  ")
+print B+("#                     LUCY MAULANA                   #")
+print W+ ("#====================================================#")
+print O+(">>>>>>>>>>>>>>>>>>>PILIH JENIS SPAM<<<<<<<<<<<<<<<<<<< ")
+print ("  ")
+print G+("   01.JDID                         ")
+print G+("   02.TELKOMSEL                    ")
+print G+("   03.PHD                          ")
+print G+("   04.Bomb Email                 ")
+print G+("   05.Call LiteOTP                         ")
+print B+("   00.KELUAR")
+print ("   ")
+print O+(">>>>>>>>>>>>>>>>>>>>>>>>THANKS<<<<<<<<<<<<<<<<<<<<<<<<<")
+Tools =raw_input("Pilih Salah Satu>>>>")
+
+if Tools == '01' or Tools == '1' :
+        print R+ ("#____________________________________________________#")
+        print W+("|                                   ___   _    _     |")
+        print R+("|         |\     /\   |  \   /     / _ \ | |  | |    |")
+        print W+("|         ||    /__\  |   \ /      || || | |__| |    |")
+        print R+("| ()#####| [====================================>>>  |")
+        print W+("|         ||   /    \ |___ /\      ||_|| |____| |    |")
+        print R+("|         |/              /  \     \___/      | |    |")
+        print W+("|                        /    \               |_|    |")
+        print R+("|                                                    |")
+        print W+("|____________________________________________________|")
+        key = raw_input("^_<Thanks Udah Menggunakan tools ini ^_^ ...TEKAN ENTER UNTUK LANJUT")
+        
+        os.system('php jdid.php')
+       
+elif Tools == '02' or Tools == '2' :
+        print R+ ("#____________________________________________________#")
+        print W+("|                                   ___   _    _     |")
+        print R+("|         |\     /\   |  \   /     / _ \ | |  | |    |")
+        print W+("|         ||    /__\  |   \ /      || || | |__| |    |")
+        print R+("| ()#####| [====================================>>>  |")
+        print W+("|         ||   /    \ |___ /\      ||_|| |____| |    |")
+        print R+("|         |/              /  \     \___/      | |    |")
+        print W+("|                        /    \               |_|    |")
+        print R+("|                                                    |")
+        print W+("|____________________________________________________|")
+        key = raw_input("^_<Thanks Udah Menggunakan tools ini ^_^ ...TEKAN ENTER UNTUK LANJUT")
+        
+        os.system('php telkomsel.php')
+        
+elif Tools == '03' or Tools == '3' :
+        print R+ ("#____________________________________________________#")
+        print W+("|                                   ___   _    _     |")
+        print R+("|         |\     /\   |  \   /     / _ \ | |  | |    |")
+        print W+("|         ||    /__\  |   \ /      || || | |__| |    |")
+        print R+("| ()#####| [====================================>>>  |")
+        print W+("|         ||   /    \ |___ /\      ||_|| |____| |    |")
+        print R+("|         |/              /  \     \___/      | |    |")
+        print W+("|                        /    \               |_|    |")
+        print R+("|                                                    |")
+        print W+("|____________________________________________________|")
+        key = raw_input("^_<Thanks Udah Menggunakan tools ini ^_^ ...TEKAN ENTER UNTUK LANJUT")
+        
+        os.system('php phd.php')
+        
+elif Tools == '04' or Tools == '4' :
+        print R+ ("#____________________________________________________#")
+        print W+("|                                   ___   _    _     |")
+        print R+("|         |\     /\   |  \   /     / _ \ | |  | |    |")
+        print W+("|         ||    /__\  |   \ /      || || | |__| |    |")
+        print R+("| ()#####| [====================================>>>  |")
+        print W+("|         ||   /    \ |___ /\      ||_|| |____| |    |")
+        print R+("|         |/              /  \     \___/      | |    |")
+        print W+("|                        /    \               |_|    |")
+        print R+("|                                                    |")
+        print W+("|____________________________________________________|")
+        key = raw_input("^_<Thanks Udah Menggunakan tools ini ^_^ ...TEKAN ENTER UNTUK LANJUT")
+        
+        os.system('python2 bombemail.py')
+        
+elif Tools == '05' or Tools == '5' :
+        print R+ ("#____________________________________________________#")
+        print W+("|                                   ___   _    _     |")
+        print R+("|         |\     /\   |  \   /     / _ \ | |  | |    |")
+        print W+("|         ||    /__\  |   \ /      || || | |__| |    |")
+        print R+("| ()#####| [====================================>>>  |")
+        print W+("|         ||   /    \ |___ /\      ||_|| |____| |    |")
+        print R+("|         |/              /  \     \___/      | |    |")
+        print W+("|                        /    \               |_|    |")
+        print R+("|                                                    |")
+        print W+("|____________________________________________________|")
+        key = raw_input("^_<Thanks Udah Menggunakan tools ini ^_^ ...TEKAN ENTER UNTUK LANJUT")
+        
+        os.system('php lite.php')
+
